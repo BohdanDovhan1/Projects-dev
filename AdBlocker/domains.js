33c2c6e33b627fb0feb8b1571bf77b8ce264edd0
BohdanDovhan2 <42681621+BohdanDovhan2@users.noreply.github.com>
2020-09-08 06:25:07 -0400

    Add files via upload

---
+++ AdBlocker/domains.js
@@ -0,0 +1,6 @@
+var blocked_domains = [
+    "*://*.lb.usemaxserver.de/*",
+    "*://*.tracking.klickthru.com/*",
+    "*://*.zv1.november-lax.com/*",
+    "*://*.zmedia.com/*",
+    "*://*.doubleclick.net/*"];
